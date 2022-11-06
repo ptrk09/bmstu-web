eac6d4d68205ff6500e54fdba8f85f60b3902356
Tema Sarkisov <sarkisov@artlebedev.ru>
2022-11-06 19:33:25 +0300

    Big update for database

--- database/data/data_csv_clean/try.py
+++ database/data/data_csv_clean/try.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d96ce8f23bfbe39fa5e8f766dfcc6359076e73942cedddde7e3f909892821e8
-size 970
+oid sha256:e9a1252796370150eb34f4bf6188d93af69e1bb3cd4413163cb07e37dde857d1
+size 3684
--- database/sql/create_tables.sql
+++ database/sql/create_tables.sql
@@ -1,31 +1,3 @@
-CREATE TABLE listings (
-    id SERIAL, 
-    name VARCHAR, 
-    user_id INTEGER,
-    neighbourhood VARCHAR,
-    apart_type_id INTEGER,
-    price FLOAT,
-    minimum_nights INTEGER,
-
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (apart_type_id) REFERENCES apart_types (id)
-);
-
--- --------------------------------------------------------------------------------------
-
-CREATE TABLE calendar (
-    id SERIAL,
-    listing_id INTEGER, 
-    date DATE, 
-    available BOOLEAN,
-
-    PRIMARY KEY (id),
-    FOREIGN KEY (listing_id) REFERENCES listings (id)
-);
-
--- --------------------------------------------------------------------------------------
-
 CREATE TABLE apart_types (
     id SERIAL,
     apart_type VARCHAR,
@@ -54,3 +26,64 @@ CREATE TABLE users (
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES roles (id)
 )
+
+-- --------------------------------------------------------------------------------------
+
+CREATE TABLE listings (
+    id SERIAL, 
+    name VARCHAR, 
+    user_id INTEGER,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+-- --------------------------------------------------------------------------------------
+
+CREATE TABLE listings_detailed (
+    id SERIAL, 
+    description TEXT,
+    neighbourhood VARCHAR,
+    apart_type_id INTEGER,
+    price FLOAT,
+    minimum_nights INTEGER,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (apart_type_id) REFERENCES apart_types (id)
+);
+
+-- --------------------------------------------------------------------------------------
+
+CREATE TABLE listings_images (
+    id SERIAL, 
+    listing_id INTEGER,
+    image_path VARCHAR,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (listing_id) REFERENCES listings (id)
+);
+
+-- --------------------------------------------------------------------------------------
+
+CREATE TABLE calendar (
+    id SERIAL,
+    listing_id INTEGER, 
+    date DATE, 
+    available BOOLEAN,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (listing_id) REFERENCES listings (id)
+);
+
+-- --------------------------------------------------------------------------------------
+
+CREATE TABLE bookings (
+    id SERIAL,
+    user_id INTEGER,
+    listing_id INTEGER, 
+    date DATE, 
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (listing_id) REFERENCES listings (id)
+);
